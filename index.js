e25af5dc2ffe24bc121eeb67dfdc4816e8fd04bc
AndrewZamora <andrewmichaelzamora@gmail.com>
2020-04-24 23:00:31 +0900

    Added multiple g tags to svg

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const div = d3.select('div');
 div.append('svg')
-d3.select('svg').append('g');
-d3.select('g').attr("id", "x-axis");
+const dataSet = ['x-axis', 'y-axis'];
+const svg = d3.select('svg');
+svg.selectAll('g').data(dataSet).enter().append('g').attr("id", d => d);
