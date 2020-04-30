51ac5e3ba43a15dcf85b693e11e5b7f2cc45035f
AndrewZamora <andrewmichaelzamora@gmail.com>
2020-04-30 23:42:10 +0900

    Added bars

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const height = 1000;
 const width = 1000;
 const svg = d3.select('div').append('svg').attr("height", height).attr("width", width);
-const dataSet = [[1, 2], [3, 4], [7, 8]];
+const dataSet = [1, 2, 3, 4, 7, 8];
 const padding = 50;
-const xScale = d3.scaleLinear().domain([0, d3.max(dataSet, (d) => d[0])]).range([padding, width - padding]);
-const yScale = d3.scaleLinear().domain([0, d3.max(dataSet, (d) => d[0])]).range([height - padding, padding]);
+const xScale = d3.scaleLinear().domain([0, d3.max(dataSet, (d) => d)]).range([padding, width - padding]);
+const yScale = d3.scaleLinear().domain([0, d3.max(dataSet, (d) => d)]).range([height - padding, padding]);
 const xAxis = d3.axisBottom(xScale);
 const yAxis = d3.axisLeft(yScale);
 svg.append("g").attr("transform", "translate(0," + (height - padding) + ")").attr("id", "x-axis").call(xAxis);
 svg.append("g").attr("transform", "translate(" + (padding) + ",0)").attr("id", "y-axis").call(yAxis);
+svg.selectAll("rect")
+    .data(dataSet)
+    .enter()
+    .append("rect")
+    .attr("x", (d, i) => i * 40)
+    .attr("y", d => height - d * 3)
+    .attr("width", 30)
+    .attr("height", (d) => d * 3)
+    .attr("class", "bar")
