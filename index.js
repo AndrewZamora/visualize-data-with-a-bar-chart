47f82047a8ffe4afa01e547f422cd073a1c151b7
AndrewZamora <andrewmichaelzamora@gmail.com>
2020-05-24 23:45:11 +0900

    Got something that resembles a bar chart

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 (async () => {
     const { data } = await (await fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json")).json();
+    console.log(data)
     const height = 1000;
     const width = 1000;
     const svg = d3.select('div').append('svg').attr("height", height).attr("width", width);
     const padding = 100;
-    const xScale = d3.scaleLinear().domain([0, d3.max(data, (d) => d[1])]).range([padding, width - padding]);
-    const yScale = d3.scaleLinear().domain([0, d3.max(data, (d) => d[1])]).range([height - padding, padding]);
-    const xAxis = d3.axisBottom(xScale);
-    const yAxis = d3.axisLeft(yScale);
-    svg.append("g").attr("transform", "translate(0," + (height - padding) + ")").attr("id", "x-axis").call(xAxis);
-    svg.append("g").attr("transform", "translate(" + (padding) + ",0)").attr("id", "y-axis").call(yAxis);
+    const xScale = d3.scaleBand().domain(data.map(d => {
+        const date = new Date(d[0]);
+        return date.getFullYear();
+    })).range([0, width]);
+    const yScale = d3.scaleLinear().domain([0, d3.max(data, (d) => d[1])]).range([0, height]);
+    // const xAxis = d3.axisBottom(xScale);
+    // const yAxis = d3.axisLeft(yScale);
+    // svg.append("g").attr("transform", "translate(0," + (height) + ")").attr("id", "x-axis").call(xAxis);
+    // svg.append("g").attr("transform", "translate(" + (0) + ",0)").attr("id", "y-axis").call(yAxis);
     svg.selectAll("rect")
         .data(data)
         .enter()
         .append("rect")
-        .attr("x", (d, i) => ((i * 60) + padding))
-        .attr("y", d => (height - d[1] * 3) - padding)
-        .attr("width", 60)
-        .attr("height", (d) => (d[1] * 3))
+        .attr('x', d => xScale((new Date(d[0]).getFullYear())))
+        .attr('y', d => height - yScale(d[1]))
+        .attr("height", d => yScale(d[1]))
+        .attr("width", xScale.bandwidth())
         .attr("class", "bar")
 })();
