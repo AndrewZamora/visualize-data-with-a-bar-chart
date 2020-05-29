6f9f27c38642e303ef58a5298c03f7c7e6be5e64
AndrewZamora <andrewmichaelzamora@gmail.com>
2020-05-30 01:31:01 +0900

    Cut down on x axis ticks and created getYear function

--- index.js
+++ index.js
@@ -8,14 +8,14 @@
     const innerWidth = chartWidth - margin.left - margin.right;
     const xScale = d3.scaleBand().rangeRound([0, innerWidth]).padding(0.1);
     const yScale = d3.scaleLinear().range([innerHeight, 0]);
-    xScale.domain(data.map(d => {
-        const date = new Date(d[0]);
-        return date.getFullYear();
-    }));
-    yScale.domain([0, d3.max(data, (d) => d[1])]);
+    const getYear = date => {
+        return new Date(date).getFullYear();
+    };
+    xScale.domain(data.map(d => getYear(d[0])));
+    yScale.domain([0, d3.max(data, d => d[1])]);
     const chartContainer = d3.select('div').append('svg').attr("height", chartHeight).attr("width", chartWidth);
     const chart = chartContainer.append('g').attr("transform", `translate(${margin.left},${margin.top})`);
-    const xAxis = d3.axisBottom(xScale);
+    const xAxis = d3.axisBottom(xScale).tickValues(xScale.domain().filter((d, i) => !(i % 4)))
     const yAxis = d3.axisLeft(yScale);
     chart
         .selectAll('.bar')
@@ -25,11 +25,10 @@
         .attr("class", "bar")
         .attr('width', xScale.bandwidth())
         .attr('height', d => innerHeight - yScale(d[1]))
-        .attr('x', d => {
-            const date = new Date(d[0]);
-            return xScale(date.getFullYear());
-        })
+        .attr('x', d => xScale(getYear(d[0])))
         .attr('y', d => yScale(d[1]))
+        .attr('data-date', d => d[0])
+        .attr('data-gdp', d => d[1])
     chart.append('g').call(xAxis).attr("id", "x-axis").attr("transform", `translate(0,${innerHeight})`);
     chart.append('g').call(yAxis).attr("id", "y-axis").attr("transform", `translate(0,0)`);
 })();
