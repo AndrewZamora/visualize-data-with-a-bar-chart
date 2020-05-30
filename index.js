558293b781d50276309aa5ffc1c5fa3bb46d0e43
AndrewZamora <andrewmichaelzamora@gmail.com>
2020-05-30 21:34:30 +0900

    Switched to timescale for x axis

--- index.js
+++ index.js
@@ -6,16 +6,13 @@
     const chartWidth = 800;
     const innerHeight = chartHeight - margin.top - margin.bottom;
     const innerWidth = chartWidth - margin.left - margin.right;
-    const xScale = d3.scaleBand().rangeRound([0, innerWidth]).padding(0.1);
+    const xScale = d3.scaleTime().range([0,innerWidth]);
     const yScale = d3.scaleLinear().range([innerHeight, 0]);
-    const getYear = date => {
-        return new Date(date).getFullYear();
-    };
-    xScale.domain(data.map(d => getYear(d[0])));
+    xScale.domain([new Date(d3.min(data, d => d[0])),new Date(d3.max(data, d => d[0]))]);
     yScale.domain([0, d3.max(data, d => d[1])]);
     const chartContainer = d3.select('div').append('svg').attr("height", chartHeight).attr("width", chartWidth);
     const chart = chartContainer.append('g').attr("transform", `translate(${margin.left},${margin.top})`);
-    const xAxis = d3.axisBottom(xScale).tickValues(xScale.domain().filter((d, i) => !(i % 4)))
+    const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale);
     chart
         .selectAll('.bar')
@@ -23,9 +20,9 @@
         .enter()
         .append('rect')
         .attr("class", "bar")
-        .attr('width', xScale.bandwidth())
+        .attr('width', 2)  
         .attr('height', d => innerHeight - yScale(d[1]))
-        .attr('x', d => xScale(getYear(d[0])))
+        .attr('x', d => xScale(new Date(d[0])))
         .attr('y', d => yScale(d[1]))
         .attr('data-date', d => d[0])
         .attr('data-gdp', d => d[1])
